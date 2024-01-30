b22fed4be0503dc40d78c043a3713f0a63fd5ac4
kamran181 <kamrannnkhan92@gmail.com>
2024-01-30 22:39:18 +0530

    added 3 sum

--- Two Pointers/programs.js
+++ Two Pointers/programs.js
@@ -102,3 +102,112 @@ let validPlain = function (s) {
   };
   console.log(twoSum3( [1, 3, 4, 5, 7, 11], 9));
   
+
+
+//@3 THREESUM          
+//   Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+// Notice that the solution set must not contain duplicate triplets.
+// Input: nums = [-1,0,1,2,-1,-4]
+// Output: [[-1,-1,2],[-1,0,1]]
+
+
+
+//3.1 this does not fullfill for every combination of an array nums fot eg inp =[0,0,0,0] op [[0,0,0],[0,0,0]]  but the op should br [[0,0,0]]
+let threeSums = function (nums) {
+   let arr =[];
+   if(nums.length<3) return arr;
+   let result =0;
+   let sortArr = nums.sort((a,b)=> a-b)
+
+   for( let i=0 ; i<sortArr.length-2 ;i++){
+    if(sortArr[i]=== sortArr[i-1]) continue;
+    let j = i+1;
+    let k = sortArr.length-1;
+    while(j<k){
+      let sum = sortArr[i] + sortArr[j] + sortArr[k]
+      if(sum === result){
+        arr.push([sortArr[i] , sortArr[j] , sortArr[k]]);
+        j++;
+
+      }
+      if(sum>result){
+        k--;
+      }
+      if(sum< result){
+        j++;
+      }
+    }
+   }
+   return arr;
+}
+
+
+console.log(threeSums( [-1,0,1,2,-1,-4]));
+
+//@3.2 Fullfills every combinations
+var threeSum = function (nums) {
+  const result = [];
+  const target = 0;
+  // we need 3 values for this to work
+  // so return an empty array if we have less than 3
+  if (nums.length < 3) {
+    return result;
+  }
+
+  // sorting is ok because the function is already O(n^2)
+  // and sort is O(nlogn)
+  // this also lets us stop iterating once weve passed the target value
+  nums = nums.sort((a, b) => a - b);
+
+  // well use i as our anchor as we move through the array
+  // we stop at nums.length - 2 to prevent undefined for k
+  for (let i = 0; i < nums.length - 2; i++) {
+    // because we sorted the array already
+    // we can stop here if the current iterator is greater than the target value
+    if (nums[i] > target) {
+      break;
+    }
+
+    // if our iterator is the same as the previous value
+    // skip it to prevent duplicate results
+    if (i > 0 && nums[i] === nums[i - 1]) {
+      continue;
+    }
+
+    // start j at i + 1
+    let j = i + 1;
+
+    // start k at end of array
+    let k = nums.length - 1;
+
+    // walking j and k towards each other to find all possible values
+    // with i as our anchor value
+    while (j < k) {
+      let sum = nums[i] + nums[j] + nums[k];
+      if (sum === target) {
+        result.push([nums[i], nums[j], nums[k]]);
+
+        // skip duplicate values of j and k
+        while (nums[j] === nums[j + 1]) j++;
+        while (nums[k] === nums[k - 1]) k--;
+
+        // move j and k inward
+        j++;
+        k--;
+        continue;
+      }
+      if (sum < target) {
+        j++;
+        continue;
+      }
+      if (sum > target) {
+        k--;
+        continue;
+      }
+    }
+  }
+  return result;
+};
+
+console.log(threeSum([-4, -2, -2, -2, 0, 1, 2, 2, 2, 3, 3, 4, 4, 6, 6]));
