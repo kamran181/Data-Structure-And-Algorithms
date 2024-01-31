68a143651c83f07645fba37e29045adaae7828ef
kamran181 <kamrannnkhan92@gmail.com>
2024-02-01 00:01:41 +0530

    added container with most water

--- Two Pointers/programs.js
+++ Two Pointers/programs.js
@@ -211,3 +211,30 @@ var threeSum = function (nums) {
 };
 
 console.log(threeSum([-4, -2, -2, -2, 0, 1, 2, 2, 2, 3, 3, 4, 4, 6, 6]));
+
+//@4   Container With Most Water
+// You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+// Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+// Return the maximum amount of water a container can store.
+
+// Notice that you may not slant the container.
+
+// Input: height = [1,8,6,2,5,4,8,3,7]
+// Output: 49
+// Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+
+let maxArea = function(height){
+  let area = 0;
+  let i =0 ;
+  let j= height.length-1;
+  while(i<j){
+    area = Math.max(area , Math.min(height[i] , height[j]) * (j-i));
+    height[i] <= height[j] ? i++ : j--
+  }
+  return area
+
+}
+
+console.log(maxArea([1,8,6,2,5,4,8,3,7]))
